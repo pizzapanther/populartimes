1b1178c1f6788b1efe3326032a9eff3617896199
Paul Bailey <paul@neutron.studio>
2021-12-23 12:07:48 -0600

    get endpoint

--- popapi/views.py
+++ popapi/views.py
@@ -14,4 +14,22 @@ def get_by_id(request):
 
 @cache_page(60 * 15)
 def get(request):
-  pass
+  types = request.GET['types'].split(',')
+  bound_lower = request.GET['bound_lower'].split(',')
+  bound_upper = request.GET['bound_upper'].split(',')
+  radius = int(request.GET.get('radius', '50000'))
+  all_places = int(request.GET.get('all_places', '0'))
+
+  bound_lower = [float(b) for b in bound_lower]
+  bound_upper = [float(b) for b in bound_upper]
+
+  ret = populartimes.get(
+    settings.MAPS_API_KEY,
+    types,
+    bound_lower,
+    bound_upper,
+    n_threads=20,
+    radius=radius,
+    all_places=all_places,
+  )
+  return http.JsonResponse({'results': ret})
