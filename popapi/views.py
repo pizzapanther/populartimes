97c39ecf96336cd23fc273bc4e6819a60c827504
Paul Bailey <paul@neutron.studio>
2021-12-23 12:27:28 -0600

    more cache

--- popapi/views.py
+++ popapi/views.py
@@ -5,14 +5,14 @@ from django.views.decorators.cache import cache_page
 import populartimes
 
 
-@cache_page(60 * 15)
+@cache_page(60 * 30)
 def get_by_id(request):
   place_id = request.GET['place_id']
   ret = populartimes.get_id(settings.MAPS_API_KEY, place_id)
   return http.JsonResponse(ret)
 
 
-@cache_page(60 * 15)
+@cache_page(60 * 30)
 def get(request):
   types = request.GET['types'].split(',')
   bound_lower = request.GET['bound_lower'].split(',')
